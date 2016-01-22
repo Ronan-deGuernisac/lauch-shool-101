b1430dc4aa507cde83fd0795ac1a6f2476654dcc
superchilled <karl@superchilled.co.uk>
2016-01-22 17:22:14 +0000

    Add bonus feature 3 - AI defence

--- lesson_4/tic_tac_toe.rb
+++ lesson_4/tic_tac_toe.rb
@@ -57,6 +57,17 @@ def empty_squares(brd)
   brd.keys.select { |num| brd[num] == INITIAL_MARKER }
 end
 
+def threat_squares(brd)
+  threat_squares = []
+  WINNING_LINES.each do |line|
+    if brd.values_at(line[0], line[1], line[2]).count(PLAYER_MARKER) == 2 &&
+       brd.values_at(line[0], line[1], line[2]).count(INITIAL_MARKER) == 1
+      line.each { |square| threat_squares << square if brd[square] == INITIAL_MARKER }
+    end
+  end
+  threat_squares
+end
+
 def player_places_piece!(brd)
   square = ''
   loop do
@@ -69,8 +80,16 @@ def player_places_piece!(brd)
 end
 
 def computer_places_piece!(brd)
-  square = empty_squares(brd).sample
+  if immediate_threat?(brd)
+    square = threat_squares(brd).sample
+  else
+    square = empty_squares(brd).sample
+  end
   brd[square] = COMPUTER_MARKER
+end
+
+def immediate_threat?(brd)
+  !threat_squares(brd).empty?
 end
 
 def board_full?(brd)
