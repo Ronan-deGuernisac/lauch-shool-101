2b2cb1b3d46af253f69477ec81a5ad3f64b91f3b
superchilled <karl@superchilled.co.uk>
2016-01-31 19:00:54 +0000

    Refactor following internal review and rubocop

--- lesson_4/21_game.rb
+++ lesson_4/21_game.rb
@@ -1,39 +1,42 @@
 # 21_game.rb
 
-require 'pry'
-
 CARD_SUITS = [
-  {suit_name: "Spades", suit_symbol: "\u2660"},
-  {suit_name: "Hearts", suit_symbol: "\u2665"},
-  {suit_name: "Clubs", suit_symbol: "\u2663"},
-  {suit_name: "Diamonds", suit_symbol: "\u2666"}
-   ]
+  { suit_name: "Spades", suit_symbol: "\u2660" },
+  { suit_name: "Hearts", suit_symbol: "\u2665" },
+  { suit_name: "Clubs", suit_symbol: "\u2663" },
+  { suit_name: "Diamonds", suit_symbol: "\u2666" }
+]
 
 CARD_VALUES = [
-  {value_name: "One", value_symbol: "1", points: 1},
-  {value_name: "Two", value_symbol: "2", points: 2},
-  {value_name: "Three", value_symbol: "3", points: 3},
-  {value_name: "Four", value_symbol: "4", points: 4},
-  {value_name: "Five", value_symbol: "5", points: 5},
-  {value_name: "Six", value_symbol: "6", points: 6},
-  {value_name: "Seven", value_symbol: "7", points: 7},
-  {value_name: "Eight", value_symbol: "8", points: 8},
-  {value_name: "Nine", value_symbol: "9", points: 9},
-  {value_name: "Ten", value_symbol: "10", points: 10},
-  {value_name: "Jack", value_symbol: "J", points: 10},
-  {value_name: "Queen", value_symbol: "Q", points: 10},
-  {value_name: "King", value_symbol: "K", points: 10},
-  {value_name: "Ace", value_symbol: "A", points: 11},
-  ]
+  { value_name: "One", value_symbol: "1", points: 1 },
+  { value_name: "Two", value_symbol: "2", points: 2 },
+  { value_name: "Three", value_symbol: "3", points: 3 },
+  { value_name: "Four", value_symbol: "4", points: 4 },
+  { value_name: "Five", value_symbol: "5", points: 5 },
+  { value_name: "Six", value_symbol: "6", points: 6 },
+  { value_name: "Seven", value_symbol: "7", points: 7 },
+  { value_name: "Eight", value_symbol: "8", points: 8 },
+  { value_name: "Nine", value_symbol: "9", points: 9 },
+  { value_name: "Ten", value_symbol: "10", points: 10 },
+  { value_name: "Jack", value_symbol: "J", points: 10 },
+  { value_name: "Queen", value_symbol: "Q", points: 10 },
+  { value_name: "King", value_symbol: "K", points: 10 },
+  { value_name: "Ace", value_symbol: "A", points: 11 }
+]
 
 HIGHEST_SCORE = 21
 DEALER_STICK_SCORE = 17
+PLAYER_OPTIONS = { 'h' => 'Hit', 's' => 'Stick' }
+
+def prompt(msg)
+  puts "=> #{msg}"
+end
 
 def initialise_deck(deck)
   CARD_SUITS.each do |suit|
     CARD_VALUES.each do |value|
       deck << {
-        suit_name: suit[:suit_name], suit_symbol: suit[:suit_symbol], 
+        suit_name: suit[:suit_name], suit_symbol: suit[:suit_symbol],
         value_name: value[:value_name], value_symbol: value[:value_symbol], points: value[:points]
       }
       deck.shuffle!
@@ -41,27 +44,39 @@ def initialise_deck(deck)
   end
 end
 
-def deal_initial_cards(deck, player_hand, dealer_hand)
+def deal_initial_cards(deck, hands)
   2.times do
-    deal_card(deck, player_hand)
-    deal_card(deck, dealer_hand)
+    hands.each { |_, hand| deal_card(deck, hand, false) }
   end
 end
 
-def deal_card(deck, hand)
-  hand << deck.shift
+def deal_card(deck, hand, announce_card=true)
+  card = deck.shift
+  hand << card
+  announce_card(card) if announce_card
 end
 
-def show_table(player_hand, dealer_hand, hidden=true)
+def show_cards(hand, hidden_card=false)
+  cards = []
+  hand.each { |card| cards << "#{card[:value_symbol]}#{card[:suit_symbol]}" }
+  cards[0] = "??" if hidden_card
+  cards.join("  ")
+end
+
+def show_score(hand, hidden_card=false)
+  score = calculate_score(hand)
+  score >= 10 ? score = "#{score}" : score = " #{score}"
+  score = "??" if hidden_card
+  score
+end
+
+def show_table(hands, dealer_hidden=true)
   system 'clear'
-  player_cards = []
-  dealer_cards = []
-  player_hand.each { |card| player_cards << "#{card[:value_symbol]}#{card[:suit_symbol]}"}
-  dealer_hand.each { |card| dealer_cards << "#{card[:value_symbol]}#{card[:suit_symbol]}"}
-  dealer_cards[0] = "??" if hidden
   puts "-----------------------------------------"
-  puts "Player: #{player_cards.join(" ")}"
-  puts "Dealer: #{dealer_cards.join(" ")}"
+  puts " PLAYER | SCORE  | CARDS"
+  puts "-----------------------------------------"
+  puts " Player |   #{show_score(hands['Player'])}   | #{show_cards(hands['Player'])}"
+  puts " Dealer |   #{show_score(hands['Dealer'], dealer_hidden)}   | #{show_cards(hands['Dealer'], dealer_hidden)}"
   puts "-----------------------------------------"
 end
 
@@ -71,7 +86,7 @@ end
 
 def calculate_score(hand)
   score = 0
-  hand.each { |card| score += card[:points]}
+  hand.each { |card| score += card[:points] }
   score = reduce_aces(hand, score) if score > HIGHEST_SCORE && ace_count(hand) > 0
   score
 end
@@ -101,57 +116,93 @@ def dealer_hit_or_stick(hand)
   calculate_score(hand) >= DEALER_STICK_SCORE ? "s" : "h"
 end
 
-def declare_winner(player_hand, dealer_hand)
-  calculate_score(player_hand) == calculate_score(dealer_hand) ? "It's a tie" : calculate_score(player_hand) > calculate_score(dealer_hand) ? "Player wins!" : "Dealer wins!"
+def invalid_choice?(choice)
+  PLAYER_OPTIONS.key?(choice)
+end
+
+def player_choice
+  hit_or_stick = ""
+  loop do
+    prompt "Hit or Stick? (type H or S)"
+    hit_or_stick = gets.chomp
+    prompt "Sorry, that's not a valid choice" if !invalid_choice?(hit_or_stick.downcase)
+    break if hit_or_stick.downcase == 'h' || hit_or_stick.downcase == 's'
+  end
+  hit_or_stick
+end
+
+def dealer_choice(hand)
+  sleep(2)
+  dealer_hit_or_stick(hand)
+end
+
+def announce_choice(player, choice)
+  prompt "#{player} chose to #{PLAYER_OPTIONS[choice]}"
+  sleep(1)
+end
+
+def announce_card(card)
+  prompt "Card dealt was a #{card[:value_name]} of #{card[:suit_name]}"
+  sleep(2)
+end
+
+def announce_bust(hands, current_player)
+  show_table(hands, false)
+  prompt "#{current_player} busted!" if busted?(hands[current_player])
+end
+
+def play_turn(deck, hands, current_player)
+  loop do
+    current_player == 'Dealer' ? show_table(hands, false) : show_table(hands)
+    current_player == 'Player' ? hit_or_stick = player_choice : hit_or_stick = dealer_choice(hands[current_player])
+    announce_choice(current_player, hit_or_stick)
+    deal_card(deck, hands[current_player]) if hit_or_stick.downcase == "h"
+    break if busted?(hands[current_player]) || hit_or_stick.downcase == "s"
+  end
+end
+
+def declare_winner(hands)
+  if busted?(hands['Player'])
+    prompt 'Dealer wins'
+  elsif busted?(hands['Dealer'])
+    prompt 'Player wins'
+  else
+    prompt decide_winner(hands)
+  end
+end
+
+def decide_winner(hands)
+  if calculate_score(hands['Player']) == calculate_score(hands['Dealer'])
+    "It's a tie"
+  elsif calculate_score(hands['Player']) > calculate_score(hands['Dealer'])
+    "Player wins!"
+  else
+    "Dealer wins!"
+  end
 end
 
 def play_again?
   gets.chomp.downcase.start_with?('y') ? true : false
 end
 
-
-
 loop do
   deck = []
   initialise_deck(deck)
-  player_hand = []
-  dealer_hand = []
-  deal_initial_cards(deck, player_hand, dealer_hand)
+  hands = { 'Player' => [], 'Dealer' => [] }
+  deal_initial_cards(deck, hands)
 
-  loop do # player loop
-    show_table(player_hand, dealer_hand)
-    puts "Hit or Stick? (type H or S)"
-    hit_or_stick = gets.chomp
-    deal_card(deck, player_hand) if hit_or_stick.downcase == "h"
-    break if busted?(player_hand) || hit_or_stick.downcase == "s"
+  play_turn(deck, hands, 'Player')
+
+  if busted?(hands['Player'])
+    announce_bust(hands, 'Player')
+  else
+    play_turn(deck, hands, 'Dealer')
+    announce_bust(hands, 'Dealer') if busted?(hands['Dealer'])
   end
-  show_table(player_hand, dealer_hand)
-  puts "You busted! Dealer wins!" if busted?(player_hand)
+  declare_winner(hands)
 
-  if !busted?(player_hand)
-    loop do # player loop
-      show_table(player_hand, dealer_hand, false)
-      puts "Hit or Stick? (type H or S)"
-      hit_or_stick = dealer_hit_or_stick(dealer_hand)
-      deal_card(deck, dealer_hand) if hit_or_stick.downcase == "h"
-      sleep(2)
-      break if busted?(dealer_hand) || hit_or_stick.downcase == "s"
-    end
-    show_table(player_hand, dealer_hand, false)
-    puts "Dealer busted! You win!" if busted?(dealer_hand)
-  end
-
-  if !busted?(player_hand) && !busted?(dealer_hand)
-    puts declare_winner(player_hand, dealer_hand)
-  end
-
-  puts "Play again?"
+  prompt "Play again?"
   break unless play_again?
 end
 
-puts "Thanks for playing 21!"
-
-
-
-
-
+prompt "Thanks for playing 21!"
